46f36e16c9fbf029c6cd816fde5e0e3e9502cafb
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-11 15:09:22 -0600

    Work on Http Connection Example

--- src/cit360/control/HttpConnectionExample.java
+++ src/cit360/control/HttpConnectionExample.java
@@ -7,8 +7,9 @@ package cit360.control;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import javax.net.ssl.HttpsURLConnection;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
@@ -20,12 +21,12 @@ public class HttpConnectionExample {
     private final String USER_AGENT = "Mozilla/5.0";
     
     // HTTP GET request
-    public void sendGet() throws Exception {
+    public void sendGet() throws IOException {
 
-	String url = "https://www.android.com/";
+	String url = "https://dodenbier-robbie.github.io/cit360/HttpConnection.html";
 
 	URL obj = new URL(url);
-	HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
 	// optional default is GET
 	con.setRequestMethod("GET");
@@ -34,21 +35,67 @@ public class HttpConnectionExample {
 	con.setRequestProperty("User-Agent", USER_AGENT);
 
 	int responseCode = con.getResponseCode();
+        System.out.println("****** Sending GET request - Test 1 ******");
 	System.out.println("\nSending 'GET' request to URL : " + url);
 	System.out.println("Response Code : " + responseCode);
 
-	BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-	String inputLine;
-	StringBuffer response = new StringBuffer();
+        if(responseCode == con.HTTP_OK) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
 
-	while ((inputLine = in.readLine()) != null) {
-		response.append(inputLine);
-	}
-	in.close();
+            while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+            }
+            in.close();
 
-	//print result
-	System.out.println(response.toString());
+            //print result
+            System.out.println(response.toString());
+        } else {
+            con.disconnect();
+            System.out.println("GET request failed");
+        }
+        
+        sendGet2();
+    }
+    
+    public void sendGet2() throws IOException {
+
+	String url = "http://www.android.com";
+
+	URL obj = new URL(url);
+	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+	// optional default is GET
+	con.setRequestMethod("GET");
+
+	//add request header
+	con.setRequestProperty("User-Agent", USER_AGENT);
+
+	int responseCode = con.getResponseCode();
+        System.out.println("\n****** Sending GET request - Test 2 ******");
+	System.out.println("\nSending 'GET' request to URL : " + url);
+	System.out.println("Response Code : " + responseCode);
+
+        if(responseCode == con.HTTP_OK) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            System.out.println(response.toString());
+        } else {
+            con.disconnect();
+            System.out.println("GET request failed. Your connection has been reset");
+       
+        }
 
     }
 }
