4193fd0f6f6e9f90dea4e519e15f55de3d760e9f
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-29 12:25:04 -0600

    Work on APC

--- src/cit360/view/MathCalculatorView.java
+++ src/cit360/view/MathCalculatorView.java
@@ -6,6 +6,7 @@
 package cit360.view;
 
 import cit360.control.ApplicationController;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -18,22 +19,27 @@ public class MathCalculatorView {
         
     }
     
-    public void getMathInput() {
+    public void getMathInput() throws IOException {
         ApplicationController delegate = new ApplicationController();
-        int number1 = 0;
-        int number2 = 0;
+        int number1;
+        int number2;
         String operator;
         
-        Scanner in = new Scanner(System.in);
-        
-        System.out.println("Enter a number: ");
-        number1 = Integer.parseInt(in.nextLine());
-        System.out.println("Enter a math operator (+, -, *, /): ");
-        operator = in.nextLine();
-        System.out.println("Enter another number: ");
-        number2 = Integer.parseInt(in.nextLine());
-        
-        System.out.print("\n" + number1 + " " + operator + " " + number2 + " = ");
-        delegate.handleMathCommands(operator, number1, number2);
+        try {
+            Scanner in = new Scanner(System.in);
+
+            System.out.println("Enter your first number: ");
+            number1 = Integer.parseInt(in.nextLine());
+            System.out.println("Enter a math operator (+, -, *, /): ");
+            operator = in.nextLine();
+            System.out.println("Enter your second number: ");
+            number2 = Integer.parseInt(in.nextLine());
+
+            System.out.print("\n" + number1 + " " + operator + " " + number2 + " = ");
+            delegate.handleMathCommands(operator, number1, number2);
+            
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
