b284177b8bdf6d4fdf2d2306ea9e852caf8a230d
Robbie Dodenbier <rdodenbier@byui.edu>
2017-11-03 09:17:06 -0600

    Work on Threads

--- src/cit360/treads/ExecutorServiceExample.java
+++ src/cit360/treads/ExecutorServiceExample.java
@@ -14,7 +14,6 @@ import java.util.concurrent.TimeUnit;
  * @author rdodenbier
  */
 
-
 public class ExecutorServiceExample {
 
     private static Future playerTwo;
@@ -23,35 +22,34 @@ public class ExecutorServiceExample {
     public void ExecutorServiceExample() throws InterruptedException, ExecutionException {
         ExecutorService executor = Executors.newFixedThreadPool(3);
 
-        Runnable threadOne = new MyThread("\nPlayerOne", 2);
+        Runnable threadOne = new MyThread("\nPlayer One", 2);
         executor.execute(threadOne);
-        for(int i = 0; i < 2; i++) {
+        for(int i = 0; i < 10; i++) {
             if(playerTwo == null) {
-                playerTwo = executor.submit(new MyThread("PlayerTwo", 10));
+                playerTwo = executor.submit(new MyThread("Player Two", 10));
             }else {
-                playerTwo = executor.submit(new MyThread("PlayerTwo", 20));
+                playerTwo = executor.submit(new MyThread("Player Two", i*15));
             }
 
             if(playerThree == null) {
-                playerThree = executor.submit(new MyThread("PlayerThree", 50));
+                playerThree = executor.submit(new MyThread("Player Three", 20));
             }else {
-                playerThree = executor.submit(new MyThread("PlayerThree", 500));
+                playerThree = executor.submit(new MyThread("Player Three", i*20));
             }    
             
             if(playerTwo.get() == null) {
                 System.out.println("-------------------------------------------------------------");
-                System.out.println(i + 1 + ") PlayerTwo terminated successfully");
+                System.out.println(i + 1 + ") ThreadTwo terminated successfully");
             } else {
                 playerTwo.cancel(true);
             }
             if(playerThree.get() == null) {
-                System.out.println(i + 1 + ") PlayerThree terminated successfully");
+                System.out.println(i + 1 + ") ThreadThree terminated successfully");
                 System.out.println("-------------------------------------------------------------");
             } else {
                 playerThree.cancel(true);
             }
-        }
-      
+        }      
         executor.shutdown();
         executor.awaitTermination(1, TimeUnit.SECONDS);
         System.out.println("All tasks are finished!");
